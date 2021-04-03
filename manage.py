2dab3850b6e1d67d5793258fdf833e251094fca8
skye-sky-a11y <qwer494766002@gmail.com>
2021-04-03 13:30:09 +0800

    初版

--- manage.py
+++ manage.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 2018-01-30 20:53:54
-# @Author  : Bayi
-# @Link    : https://blog.flywinky.top/
 import rules
 import os
 import re
@@ -147,54 +144,6 @@ class checkJSON(object):
 # email: /^\w+@\w+\.\w+$/
 
 
-# @app.route("/joinus", methods=['POST'])
-# def joinus():
-#     data = request.get_json(force=True)
-#     # data = {'InfoName': '折蓉蓉', 'InfoPho': '13466777707','InfoProfess': '数学学院','InfoCls': '大一','InfoEmail':
-#     # '266455@qq.com', 'InfoGroup': ['移动', '运营'], 'InfoPower': '测试'}
-#     if data:
-#         addGroup = ",".join(data['InfoGroup'])
-#         addInfos = hostInfos(
-#             name=data['InfoName'],
-#             phone=data['InfoPho'],
-#             profess=data['InfoProfess'],
-#             grade=data['InfoCls'],
-#             email=data['InfoEmail'],
-#             group=addGroup,
-#             power=data['InfoPower']
-#         )
-#         db.session.add(addInfos)
-#         db.session.commit()
-#         return jsonify({"status": True})
-#     else:
-#         return jsonify({"status": False})
-
-#生成token 入参：用户name
-# def generate_token(key, expire=3600):
-#     ts_str = str(time.time() + expire)
-#     ts_byte = ts_str.encode("utf-8")
-#     sha1_tshexstr  = hmac.new(key.encode("utf-8"),ts_byte,'sha1').hexdigest() 
-#     token = ts_str+':'+sha1_tshexstr
-#     b64_token = base64.urlsafe_b64encode(token.encode("utf-8"))
-#     return b64_token.decode("utf-8")
-# #验证token 入参：用户name 和 token
-# def certify_token(key, token):
-#     token_str = base64.urlsafe_b64decode(token).decode('utf-8')
-#     token_list = token_str.split(':')
-#     if len(token_list) != 2:
-#         return False
-#     ts_str = token_list[0]
-#     if float(ts_str) < time.time():
-#         # token expired
-#         return False
-#     known_sha1_tsstr = token_list[1]
-#     sha1 = hmac.new(key.encode("utf-8"),ts_str.encode('utf-8'),'sha1')
-#     calc_sha1_tsstr = sha1.hexdigest()
-#     if calc_sha1_tsstr != known_sha1_tsstr:
-#         # token certification failed
-#         return False
-#     # token certification success
-#     return True
 @auth.verify_password
 def verify_password(name_or_token, password):
     if not name_or_token:
